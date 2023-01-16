5447758f580e88c7ed108521e475c57175a9190c
Shane Bell <rsbcard117@gmail.com>
2023-01-15 20:29:31 -0500

    added background colors to represent board and put in alternating turns

--- connect_4.js
+++ connect_4.js
@@ -10,21 +10,41 @@ let board_array = [
 
 let col_array = [35, 36, 37, 38, 39, 40, 41];
 
+let turn = 1;
+
 const col_btn = document.querySelectorAll(".drop_btn");
+const spaces = document.querySelectorAll(".circle");
 
 for (let i = 0; i < col_btn.length; i++) {
     col_btn[i].addEventListener("click", function () {
         drop_token(i);
+        add_color();
     });
 }
 
 function drop_token(col) {
     if (col_array[col] >= 0) {
-        board_array[col_array[col]] = "x";
+        if (turn == 1) {
+            board_array[col_array[col]] = "x";
+            turn = 0;
+        } else {
+            board_array[col_array[col]] = "o";
+            turn = 1;
+        }
         col_array[col] = col_array[col] - 7;
         console.log(col_array);
         console.log(board_array);
     } else {
         col_btn[col].removeEventListener("click", drop_token);
+    }
+}
+
+function add_color() {
+    for (let i = 0; i < board_array.length; i++) {
+        if (board_array[i] == "x") {
+            spaces[i].style.backgroundColor = "red";
+        } else if (board_array[i] == "o") {
+            spaces[i].style.backgroundColor = "yellow";
+        }
     }
 }
